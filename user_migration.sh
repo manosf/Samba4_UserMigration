2e28a420c0ee58b5fa67af9da3fb6410cc2218af
Dimitris Tsioumas <tsioumas@protonmail.com>
2017-10-20 20:47:19 +0300

    Extract users into a file using a RegEx

--- user_migration.sh
+++ user_migration.sh
@@ -2,6 +2,7 @@
 
 FILE="ldapusers.txt"
 LINECOUNT=0
+REGEXP="^p[0-9]|^mpsp[0-9]|^mppl[0-9]"
 
 if [ "$EUID" != "0" ]; 
 then
@@ -19,6 +20,13 @@ do
 			FILE=$2;
 			shift 2
 			;;
+		--get-users|-g)
+			GETUSERS="1";
+			;;
+        --pattern|-p)
+            REGEXP=$3;
+            shift 2
+            ;;
 	esac
 done
 
@@ -27,9 +35,17 @@ then
     printf "Options and arguments:\n"
 	echo "-h | --help Displays a usage message and exits."
     echo "-f | --file Specifies the file in which the users exist (default: ldapusers.txt)."
+    echo "-g | --get-users Export users with their fullname into a txt file (default: ldapusers.txt) using a RegEx"
+    echo "-p | --pattern Specifies the Regular Expersion you want to use to collect User data into a file"
     exit 1
 fi
 
+if [ "$GETUSERS" == "1" ]
+then
+    echo "Collecting all Users into ${FILE} "
+    getent passwd | grep -E ${REGEXP} | cut -f 1,5 -d :| sort > ${FILE}
+    exit 1
+fi
 passwd_gen()
 {
 while read -r LINE|| [[ -n "${LINE}" ]];
